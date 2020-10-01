b25b3c989c2cb55d834ef16f52e7e6893c2106e3
odinbeta <odinbeta420@gmail.com>
2020-10-01 15:06:45 -0400

    intro effect with gsap we have new pictures

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,21 @@
-gsap.fromTo("#logo", {y: -15, opacity: 0}, {duration: 1, y: 0, opacity: 1});
+gsap.from(".fade-in", {
+    y: -15, 
+    opacity: 0,
+    duration: 1,
+    stagger: 0.2,
+    delay: 3
+});
+
+gsap.from(".fadeAsset", {
+    opacity:0,
+    duration: 1,
+    stagger: 0.5,
+    delay:2
+});
+
+gsap.to(".spinner-wrapper", {
+    opacity:0,
+    duration: 1,
+    // stagger: 0.5,
+    delay:2
+});
