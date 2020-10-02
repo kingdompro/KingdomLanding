0476467653af116cc5420465d3ebbdcf87f9ce9f
odinbeta <odinbeta420@gmail.com>
2020-10-02 12:15:28 -0400

    preloader screen and rearranging photos

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,33 @@
-gsap.from(".fade-in", {
-    y: -15, 
-    opacity: 0,
-    duration: 1,
-    stagger: 0.2,
-    delay: 3
-});
 
-gsap.from(".fadeAsset", {
-    opacity:0,
-    duration: 1,
-    stagger: 0.5,
-    delay:2
-});
 
-gsap.to(".spinner-wrapper", {
-    opacity:0,
-    duration: 1,
-    // stagger: 0.5,
-    delay:2
+// gsap.to(".spinner-wrapper", {
+//     opacity:0,
+//     duration: 1,    
+//     // stagger: 0.5,
+//     // delay:2
+    
+// });
+
+window.addEventListener('load', function(event){
+    gsap.to(".spinner-wrapper", {
+        opacity:0,
+        duration: 1  
+        // stagger: 0.5,
+        // delay:2       
+    });
+    const swrapper = document.querySelector(".spinner-wrapper");    
+    swrapper.style.zIndex = -4;
+    gsap.from(".fadeAsset", {
+        opacity:0,
+        duration: 1,
+        stagger: 0.5,
+        // delay:0
+    });    
+    gsap.from(".fade-in", {
+        y: -15, 
+        opacity: 0,
+        duration: 1,
+        stagger: 0.3,
+        delay: 2
+    });
 });
